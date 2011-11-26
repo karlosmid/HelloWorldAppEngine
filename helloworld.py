0c08a283128573c5ff8194eac981e09ac3548d68
Karlo Smid <karlo.smid@gmail.com>
2011-11-26 17:14:36 +0100

    finished second part of tutorial. Added raise to check debug mode printout

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,18 @@
-import random
-print 'Content-Type: text/plain'
-print ''
-print 'Bok Karlo, kak si?'
-print random.gauss(180,5)
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+        raise TypeError
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
